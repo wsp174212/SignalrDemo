ee71a8bd99cd31bb15c55d81ef9293478f90cc5d
wsp174212 <1742129616@qq.com>
2024-08-28 16:28:48 +0800

    Raw Project Push

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Identity;
+
+using Microsoft.Azure.SignalR;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR().AddAzureSignalR(option =>
+{
+    // Here are sample codes for sovereign clouds
+    //
+    // var credentialOptions = new ClientSecretCredentialOptions()
+    // {
+    //     AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
+    //     // AuthorityHost = AzureAuthorityHosts.AzureGovernment, // for fairfax region
+    //     // AuthorityHost = AzureAuthorityHosts.AzureChina, // for mooncake region
+    // };
+
+    // var tenantId = "";
+    // var clientId = "";
+    // var clientSecret = "";
+
+    // option.Endpoints = new ServiceEndpoint[] {
+    //     new(new Uri("https://<hostname>"), new ClientSecretCredential(tenantId, clientId, clientSecret, credentialOptions))
+    // };
+});
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapHub<ChatSampleHub>("/chat");
+
+app.Run();
